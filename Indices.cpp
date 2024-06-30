cfe4a8dfb61adc8f197ab2da2b87e1098a40c6e3
Sharathcuw921 <sharathchandra.podile@cuw.edu>
2024-06-30 03:41:17 -0500

    Initial commit

---
+++ Indices.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], i};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    int n, target;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter the target value: ";
+    cin >> target;
+
+    vector<int> result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Output: [" << result[0] << "," << result[1] << "]" << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
